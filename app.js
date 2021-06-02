30754aa84b7eb63cc3b1be9e22662f723a0490c9
rawnaqaburumman <>
2021-06-02 14:46:14 +0000

    adding new js file

---
+++ app.js
@@ -0,0 +1,18 @@
+alert('Welcome to Our page, we will help you to find graet books please press Ok  (: ') 
+var  kind = prompt('Now write your favorite kind of books from these kinds (Classics,Comic,Fantasy,Romance) And look to the suggesions at the bottom of the page please.')
+if (kind=="Classics"){
+
+document.write('<div>' + 'Classics Books :Treasure Island-The Call of the Wild-Charlotte’s Web' + "</div>")}
+else if 
+(kind=="Comic"){
+
+document.write('<div>' + 'Comic Books:  Thorns-Charlotte’s Web-The United States of Captain America-Killer Queens' + "</div>")}
+else if 
+(kind=="Fantasy"){
+
+document.write('<div>' + 'Fantasy Books: THE ARABIAN NIGHTS-The Two Towers-The Return of the King' + "</div>")}
+else if 
+
+(kind=="Romance"){
+
+document.write('<div>' + 'Romance Books: The Proposal-Vision In White-Beautiful Disaster' + "</div>")}
